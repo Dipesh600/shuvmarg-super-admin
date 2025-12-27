bfafbb6d5eb4d9800ae7bea83fd02461831f86f4
kritam dahal <113635777+Kritam123@users.noreply.github.com>
2025-12-27 20:39:22 +0545

    fix: update API URL to use environment variable for better configuration management

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { api } from "./axios";
-const apiUrl = "http://34.229.93.103";
+// const apiUrl = "http://34.229.93.103";
+const apiUrl = import.meta.env.VITE_API_URL;
 export type Admin = {
   id: string;
   adminId: string;
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -9,7 +9,9 @@ const processQueue = (error: any, token: string | null = null) => {
   );
   failedQueue = [];
 };
-const apiUrl = "http://34.229.93.103";
+// const apiUrl = "http://34.229.93.103";
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
   baseURL: `${apiUrl}/api/admin`,
   withCredentials: true, // 🔑 refresh token cookie
